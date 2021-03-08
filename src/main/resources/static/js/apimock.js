d7c36392c21dc5f9b0f0ff6674bd98249bc2e3d7
Federico Barrios <federico.barrios@mail.escuelaing.edu.co>
2021-03-08 16:14:00 -0500

    Parte de la clase terminada.

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -1,41 +1,19 @@
-//@author hcadavid
-
 apimock=(function(){
-
-	var mockdata=[];
-
-    mockdata["-"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
-     {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
-
-    mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
-     {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
-
-    mockdata["federico"]=[{author:"federico","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house3"},
-         {author:"federico","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear3"}];
-
-    mockdata["guillermo"]=[{author:"guillermo","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house4"},
-         {author:"guillermo","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear4"}];
-
-
+    var mockdata=[];
+    mockdata["johnconnor"]= [{author: "johnconnor",points: [{"x": 150,"y": 120},{"x": 215,"y": 115}],name: "house"},{author: "johnconnor",points: [{"x": 340,"y": 240},{"x": 15,"y": 215}],name: "gear"}];
+    mockdata["maryweyland"]= [{author: "maryweyland",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "house2"},{author: "maryweyland",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "gear2"}];
+    mockdata["federico"]= [{author: "federico",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "house3"},{author: "federico",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "gear3"}];
+    mockdata["guillermo"]= [{author: "guillermo",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "house4"},{author: "guillermo",points: [{"x": 140,"y": 140},{"x": 115,"y": 115}],name: "gear4"}];
     return {
-        getBlueprintsByAuthor:function(authname,callback){
-            callback(
-                mockdata[authname]
-            );
-        },
-
-        getBlueprintsByNameAndAuthor:function(authname,bpname,callback){
-            callback(
-                mockdata[authname].find(function(e){return e.name===bpname})
+        getBlueprintsByAuthor:function(name,callback){
+            return callback(
+                mockdata[name]
             );
         }
-    }
+//        getBlueprintsByNameAndAuthor:function(name,bpname,callback){
+//            return callback(
+//                mockdata[name].find(function(e){return e.name===bpname})
+//            );
+//        }
+    };
 })();
-
-/*
-Example of use:
-var fun=function(list){
-	console.info(list);
-}
-apimock.getBlueprintsByAuthor("johnconnor",fun);
-apimock.getBlueprintsByNameAndAuthor("johnconnor","house",fun);*/
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,40 +1,48 @@
-var app = (function () {
-        var autor = "";
-        var listaPlanos = [];
+app = (function () {
+    function getByAuthor(funcion) {
 
-        var mapObjetos = (planos) => {
-            listaPlanos = planos.map(({author, points, name}) => ({
-                    author: planos.author,
-                    points: planos.points.length,
-                    name: planos.name
-                })
-            )
-            $("#tabla > tbody").empty();
-            listaPlanos.forEach(({author, points, name}) => {
-                $("#tabla > tbody").append(
-                    `<tr>
-                    <td> ${author} </td>
-                    <td> ${points} </td>
-                    <td> ${name} </td>
-                    <td> <button type="button" > Open </button></td>
-                    </tr>`
-                );
-            });
-        }
-
-        return {
-            actualizarPlanos() {
-                if ($("#author").val() === "") {
-                    alert("Debe ingresar un nombre !");
-                } else {
-                    this.cambiarAutor($("#author").val());
-                    apimock.getBlueprintsByAuthor(autor, mapObjetos);
-                }
-            },
-
-            cambiarAutor(author) {
-                autor = author;
-            }
-        }
+        return funcion.map(function(f){
+            return {name:f.name,points:Object.keys(f.points).length};
+        });
     }
-)();
+
+    function getBlueprints(funcion){
+        return funcion;
+    }
+
+    function getBlueprintsByNameAndAuthor(funcion,name){
+        var points=[]
+        funcion.map(function(f){
+            if(f.name==name){
+                points=f.points
+            }
+        });
+        return points;
+    }
+
+    function run() {
+        var nameAutor = $('#autor').val();
+        generarTable(nameAutor,apimock.getBlueprintsByAuthor(nameAutor,getByAuthor));
+    }
+
+    function generarTable(name,funcion) {
+        $("#cuerpo").html("");
+        var total=0
+        funcion.map(function(f) {
+            $('#cuerpo')
+                .append(
+                  `<tr>
+                    <td>`+f.name+`</td>
+                    <td>`+f.points+`</td>`+
+                    "<td><form><button type='button'>Open</button></form></td>"+
+                  `</tr>`
+                );
+                total+=f.points
+        });
+    }
+
+    return {
+        run: run,
+    }
+
+})();
